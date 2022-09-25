b9ce129b4e1f90fcbf1327df21fe8822f4d56869
miss-eronwu <90726859+miss-eronwu@users.noreply.github.com>
2022-09-25 14:45:56 -0400

    Add files via upload

---
+++ height_width.py
@@ -0,0 +1,17 @@
+#Program that lets you know how many cans of paint are needed for
+# a painting project
+
+import math
+
+
+def paint_calc(height, width, cover):
+    area = height * width
+    num_of_cans = math.ceil(area / cover)
+    print(f"You'll need {num_of_cans} cans of paint.")
+
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
